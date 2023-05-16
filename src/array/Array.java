d42a95442c4ec36407679ebfffaf4bb118190d67
Yaksh Patel <payakshk@sheridancollege.ca>
2023-05-16 13:59:43 -0400

    First Time commit

---
+++ src/array/Array.java
@@ -0,0 +1,33 @@
+
+package array;
+
+import java.util.Scanner;
+
+
+public class Array {
+
+   
+    public static void main(String[] args) {
+     /*  char[] myLetters = new char[5];
+       myLetters[0]='Y';
+       myLetters[1]='A';
+       myLetters[2]='K';
+       myLetters[3]='S';
+       myLetters[4]='H';
+       
+       for(int i=0; i<myLetters.length; i++)
+          System.out.print(myLetters[i]); */
+        System.out.println("Enter Word:");
+        Scanner sc=new Scanner(System.in);
+        String myWord=sc.nextLine();
+         char[] myLetters = new char[myWord.length()];
+         for(int i=0;i<myLetters.length;i++){
+             myLetters[i]=myWord.charAt(i);
+             System.out.print(myLetters[i]);
+    }
+         System.out.println("entering in reverse");
+         for(int i=myLetters.length-1;i>=0;i--)
+             System.out.print(myLetters[i]);
+    
+}
+}
