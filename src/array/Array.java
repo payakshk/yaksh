eae34f592ae9718e001dde046d5dff95c01e3948
payakshk <133809291+payakshk@users.noreply.github.com>
2023-05-23 13:36:53 -0400

    Update Array.java

--- src/array/Array.java
+++ src/array/Array.java
@@ -9,6 +9,7 @@ public class Array {
    
     public static void main(String[] args) {
         // added comment
+       // added comment for fetch
      /*  char[] myLetters = new char[5];
        myLetters[0]='Y';
        myLetters[1]='A';
